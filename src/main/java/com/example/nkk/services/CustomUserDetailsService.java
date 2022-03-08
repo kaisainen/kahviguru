a9b69b3a5e72edc104056d0f5a01bca562851e6b
kaisainen <kaisainen@gmail.com>
2022-03-08 16:15:56 +0200

    add userdetailsservice and dependency in pom

---
+++ src/main/java/com/example/nkk/services/CustomUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.example.nkk.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.nkk.models.Account;
+import com.example.nkk.repositories.AccountRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AccountRepo accountRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepo.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException("No such user:" + username);
+        }
+
+        List<SimpleGrantedAuthority> authorizations = new ArrayList<>();
+        for (String authority : account.getAuthorities()) {
+            authorizations.add(new SimpleGrantedAuthority(authority));
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                account.getUsername(),
+                account.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                authorizations);
+
+    }
+}
